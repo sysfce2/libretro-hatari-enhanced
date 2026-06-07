d7703b71066d0f7c30d1552fbff3d9fc85c61d7b
metallic77 <43163462+metallic77@users.noreply.github.com>
2026-06-07 13:36:25 +0300

    Update libretro.c

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -28,8 +28,8 @@ cothread_t emuThread;
 int CROP_WIDTH;
 int CROP_HEIGHT;
 int VIRTUAL_WIDTH ;
-int retrow=352; 
-int retroh=240;
+int retrow=392; 
+int retroh=248;
 
 extern unsigned short int bmp[1024*1024];
 extern int STATUTON, SHOWKEY, SHIFTON, MOUSEMODE, PAS, SND;
