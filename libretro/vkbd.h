c82575919133f31a77c817b4d062ffe2634bcd03
metallic77 <43163462+metallic77@users.noreply.github.com>
2026-06-09 21:25:54 +0300

    Update vkbd.h

--- libretro/vkbd.h
+++ libretro/vkbd.h
@@ -1,7 +1,28 @@
 #ifndef VKBD_H
-#define VKBD_H 1
+#define VKBD_H
 
-void virtual_kdb(unsigned short int *pixels,int vx,int vy);
-int check_vkey2(int x,int y);
+#include "graph.h"
 
-#endif
+extern bool vkbd_active;
+extern bool retro_capslock;
+extern signed char vkbd_ready;
+
+extern void print_vkbd(void);
+extern void input_vkbd(void);
+extern void toggle_vkbd(void);
+
+extern unsigned int opt_vkbd_theme;
+extern unsigned int opt_vkbd_alpha;
+
+extern int vkflag[10];
+
+#define VKBDX 11
+#define VKBDY 8
+
+/* Special key values */
+#define VKBD_PAGE2    -2
+#define VKBD_HIDE     -3
+#define VKBD_STAT     -4
+#define VKBD_COLOR    -5
+
+#endif /* VKBD_H */
